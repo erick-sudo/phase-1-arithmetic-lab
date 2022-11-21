8094241f11ea061bc9fed5e56b414664f5ba4dca
Erick Ochieng <erickochieng766@gmail.com>
2022-11-21 17:39:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 10
+const num4 = 34
+const multiply = num1 * num2
+const mod = num4%num3
+const random = Math.ceil(Math.random())
+const arr = [2, 15, 20, 12, 9]
+const max = Math.max(...arr)
